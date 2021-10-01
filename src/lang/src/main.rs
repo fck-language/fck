c800b3a94146f45fb74049162d4124e2aa661f92
RosiePuddles <rosiegbartlett@gmail.com>
2021-10-01 21:26:57 +0100

    Added fckl language files

---
+++ src/lang/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::{File, write};
+use std::path::Path;
+use core::slice::Iter;
+use std::io::Write;
+
+pub mod keywords;
+pub mod en;
+pub mod fr;
+pub mod de;
+
+fn csv(vector: Vec<&str>) -> String {
+    let init = String::from(*vector.first().unwrap());
+    vector[1..].iter().fold(init, |acc, arg| format!("{},{}", acc, arg))
+}
+
+fn write_formatted(code: &str, keywords: keywords::Keywords, messages: keywords::Messages) {
+    let mut file_to_write_to = File::create(format!("./fckl equivelents/{}.fckl", code)).unwrap();
+    let mut str = String::new();
+    str += &*csv(keywords.keywords.to_vec());
+    str += "\n";
+    str += &*csv(keywords.var_keywords.to_vec());
+    str += "\n";
+    str += &*csv(keywords.config_keys.to_vec());
+    str += "\n";
+    str += &*csv(messages.generic.to_vec());
+    file_to_write_to.write(str.as_ref());
+}
+
+fn main() {
+    write_formatted("en", en::KEYWORDS, en::MESSAGES);
+    write_formatted("de", de::KEYWORDS, de::MESSAGES);
+    write_formatted("fr", fr::KEYWORDS, fr::MESSAGES)
+}
