6cee77e5c22ed7b4ba0cc133cb928c88f131c892
nxe <rosiegbartlett@gmail.com>
2022-04-25 16:33:29 +0100

    changed language test to check all permutation pairs

--- tests/language.rs
+++ tests/language.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use fck;
 use fck::{
 	ast::Lexer,
@@ -6,40 +7,25 @@ use fck::{
 use lang::*;
 
 #[test]
-fn de() {
-	let toks_en = Lexer::new(en::KEYWORDS.keywords.join("\n"), en::KEYWORDS, "en".to_string()).make_tokens().unwrap();
-	let toks_de = Lexer::new(de::KEYWORDS.keywords.join("\n"), de::KEYWORDS, "de".to_string()).make_tokens().unwrap();
-	toks_en.iter().zip(toks_de.iter()).map(
-		|(t_en, t_de)| {
-			assert_eq!(t_en.type_, t_de.type_);
-			assert_eq!(t_en.pos_start, t_de.pos_start);
-			assert_eq!(t_en.pos_end.ln, t_de.pos_end.ln)
+fn language_equivalence() {
+	for t in vec![
+		(en::KEYWORDS, "en"),
+		(de::KEYWORDS, "de"),
+		(fr::KEYWORDS, "fr"),
+		(ko::KEYWORDS, "ko")
+	].iter().combinations(2) {
+		let k0 = t.first().unwrap().0;
+		let n0 = t.first().unwrap().1.to_string();
+		let k1 = t.last().unwrap().0;
+		let n1 = t.last().unwrap().1.to_string();
+		let t0 = Lexer::new(k0.keywords.join("\n"), k0, n0).make_tokens().unwrap();
+		let t1 = Lexer::new(k1.keywords.join("\n"), k1, n1).make_tokens().unwrap();
+		t0.iter().zip(t1.iter()).map(
+		|(t_0, t_1)| {
+			assert_eq!(t_0.type_, t_1.type_);
+			assert_eq!(t_0.pos_start, t_1.pos_start);
+			assert_eq!(t_0.pos_end.ln, t_1.pos_end.ln)
 		}
 	);
-}
-
-#[test]
-fn fr() {
-	let toks_en = Lexer::new(en::KEYWORDS.keywords.join("\n"), en::KEYWORDS, "en".to_string()).make_tokens().unwrap();
-	let toks_de = Lexer::new(fr::KEYWORDS.keywords.join("\n"), fr::KEYWORDS, "fr".to_string()).make_tokens().unwrap();
-	toks_en.iter().zip(toks_de.iter()).map(
-		|(t_en, t_de)| {
-			assert_eq!(t_en.type_, t_de.type_);
-			assert_eq!(t_en.pos_start, t_de.pos_start);
-			assert_eq!(t_en.pos_end.ln, t_de.pos_end.ln)
-		}
-	);
-}
-
-#[test]
-fn ko() {
-	let toks_en = Lexer::new(en::KEYWORDS.keywords.join("\n"), en::KEYWORDS, "en".to_string()).make_tokens().unwrap();
-	let toks_de = Lexer::new(ko::KEYWORDS.keywords.join("\n"), ko::KEYWORDS, "ko".to_string()).make_tokens().unwrap();
-	toks_en.iter().zip(toks_de.iter()).map(
-		|(t_en, t_de)| {
-			assert_eq!(t_en.type_, t_de.type_);
-			assert_eq!(t_en.pos_start, t_de.pos_start);
-			assert_eq!(t_en.pos_end.ln, t_de.pos_end.ln)
-		}
-	);
+	}
 }
